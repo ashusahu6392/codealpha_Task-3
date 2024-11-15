1cd226ac86004d010c875ce8be77ef2a2c9fa822
ashusahu6392 <145501021+ashusahu6392@users.noreply.github.com>
2024-11-15 15:15:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-content");
+
+function opentab(tabname){
+    for(tablinks of tablinks){
+        tablink.classList.remove("active-link");
+
+    }
+    for(tabcontents of tabcontents){
+        tabcontent.classList.remove("active-tab");
+        
+    }
+}
